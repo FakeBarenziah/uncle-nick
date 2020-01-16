49b07f43ebb7d4ff31326fbaffa8e36845dd3cca
Nick <nicholas.balestrieri@gmail.com>
2020-01-16 15:52:20 -0500

    Update for new components

--- App.js
+++ App.js
@@ -1,19 +1,42 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View, StatusBar } from 'react-native'
+import * as Font from 'expo-font'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import Splash from './components/Splash.js'
+import Login from './components/Login.js'
+
+export default class App extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      fontLoaded:false
+    }
+    
+  }
+  
+  async componentDidMount(){
+    await Font.loadAsync({
+      'open-sans' : require('./assets/fonts/OpenSans-Regular.ttf')
+    })
+
+    this.setState({fontLoaded: true})
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content"/>
+        {this.state.fontLoaded ? <Splash/> : null}
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#d1f0d5',
+    backgroundColor: '#820',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 });
